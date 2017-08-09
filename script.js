eae673d76acbea7f06295c29b218c4729d72b93b
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-08-09 22:50:49 +0400

    update dataset

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-var w = 400;
-var h = 500;
-
-var monthlySales = [
-    { "month": 1, "sales": 20 },
-    { "month": 2, "sales": 14 },
-    { "month": 3, "sales": 20 },
-    { "month": 4, "sales": 21 },
-    { "month": 5, "sales": 15 },
-    { "month": 6, "sales": 22 },
-    { "month": 7, "sales": 9 },
-    { "month": 8, "sales": 6 },
-    { "month": 9, "sales": 23 },
-    { "month": 10, "sales": 7 },
-    { "month": 11, "sales": 27 },
-    { "month": 12, "sales": 11 }
-]
+var width = 800;
+var height = 300;
+var margin = { top: 20, bottom: 20, left: 20, right: 20 };
+var dataset = [
+    { date: new Date(2007, 3, 24), value: 3.24 },
+    { date: new Date(2007, 3, 25), value: 85.35 },
+    { date: new Date(2007, 3, 26), value: 78.84 },
+    { date: new Date(2007, 3, 27), value: 99.92 },
+    { date: new Date(2007, 3, 30), value: 29.80 },
+    { date: new Date(2007, 4, 1), value: 69.47 },
+];
 
 var line = d3.line()
     .x(function (d) { return d.month })
     .y(function (d) { return h - d.sales })
-    .curve(d3.curveLinear);
 
 var svg = d3.select("body")
     .append("svg")
     .attr({
         "width": w,
         "height": h
-    });               
+    });
+
+var path = d3.select("svg").append("path")
+    .attr({
+        d: line(monthlySales),
+        "fill": "none",
+        "stroke": "blue"
+    });
+
+var label = d3.select("svg").selectAll("text")
+    .data(monthlySales)
+    .enter()
+    .append("text")
+    .text(function (d) {
+        return d.sales;
+    })
+    .attr({
+        x: function (d) {
+            return d.month * 4;
+        },
+        y: function (d) {
+            return h - d.sales;
+        }
+    })
